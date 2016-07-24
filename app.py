c2d0a24eb9cbbe7e6e8bb3737440a4550bdde923
Ahmad Hayam <ahayam.tugas1@gmail.com>
2016-07-24 23:09:52 +0700

    change template to flask

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+
+PATH_NAME = 'OPENSHIFT_PYTHON_DIR'
+virtenv = os.environ.get(PATH_NAME, '') + 'virtenv/venv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+
+try:
+	exec(compile(open(virtualenv, 'rb').read(), virtualenv, 'exec'), dict(__file__=virtualenv))
+except:
+	pass
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(port = 5000, debug = True)
--- wsgi.py
+++ wsgi.py
@@ -1,31 +1,13 @@
 #!/usr/bin/env python
 import os
 
-def application(environ, start_response):
+from app import app as application
 
-		ctype = 'text/plain'
-		if environ['PATH_INFO'] == '/health':
-				response_body = "1"
-		elif environ['PATH_INFO'] == '/env':
-				response_body = ['%s: %s' % (key, value)
-										for key, value in sorted(environ.items())]
-				response_body = '\n'.join(response_body)
-		else:
-				ctype = 'text/html'
-				response_body = 'Ahmad Hayam'
-		response_body = response_body.encode('utf-8')
+ip = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
+port = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 5000))
+host_name = os.environ.get('OPENSHIFT_GEAR_DNS', 'localhost')
 
-		status = '200 OK'
-		response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-		#
-		start_response(status, response_headers)
-		return [response_body ]
-
-#
-# Below for testing only
-#
 if __name__ == '__main__':
-		from wsgiref.simple_server import make_server
-		httpd = make_server('localhost', 8051, application)
-		# Wait for a single request, serve it and quit.
-		httpd.handle_request()
+	from wsgiref.simple_server import make_server
+	httpd = make_server(ip, port, app = application)
+	httpd.serve_forever()
