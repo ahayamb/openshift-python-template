fe326fd1b7584e488c63c4d1f57994037f88d882
Ahmad Hayam <ahayam.tugas1@gmail.com>
2016-07-24 23:34:35 +0700

    experiment with flask

--- wsgi.py
+++ wsgi.py
@@ -8,6 +8,7 @@ port = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 5000))
 host_name = os.environ.get('OPENSHIFT_GEAR_DNS', 'localhost')
 
 if __name__ == '__main__':
-	from wsgiref.simple_server import make_server
-	httpd = make_server(ip, port, app = application)
-	httpd.serve_forever()
+	# from wsgiref.simple_server import make_server
+	# httpd = make_server(ip, port, app = application)
+	# httpd.serve_forever()
+	application.run(port = port, debug = True)
